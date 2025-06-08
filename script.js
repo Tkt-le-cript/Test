6afe9b9844c8bf36b5d50e986122012449b00aad
Tkt-le-cript <ikg123454@gmail.com>
2025-06-08 15:47:23 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+const filters = document.querySelectorAll('.filter');
+filters.forEach(button => {
+  button.addEventListener('click', () => {
+    filters.forEach(btn => btn.classList.remove('active'));
+    button.classList.add('active');
+  });
+});
